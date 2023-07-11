a4fcf52b9b0d9bf42eec56d21bca75e5eb9af5db
Chendy Lundy <chendy.lundy.ft21@mail.umy.ac.id>
2023-07-11 14:57:07 +0700

    mengubah tampilan halaman utama

--- Sewa_Alat_Camping_C15/Form2.Designer.cs
+++ Sewa_Alat_Camping_C15/Form2.Designer.cs
@@ -38,7 +38,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
-            this.lblUser = new System.Windows.Forms.Label();
             this.panelSidebar = new System.Windows.Forms.FlowLayoutPanel();
             this.btnLogin = new Guna.UI2.WinForms.Guna2Button();
             this.btnMaster = new Guna.UI2.WinForms.Guna2Button();
@@ -49,12 +48,8 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
             this.btnHead.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panelHsl.SuspendLayout();
@@ -63,13 +58,11 @@
             this.panelMaster.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(102)))), ((int)(((byte)(71)))));
-            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(8, 5);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
@@ -80,6 +73,7 @@
             // btnHead
             // 
             this.btnHead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(64)))), ((int)(((byte)(40)))));
+            this.btnHead.Controls.Add(this.pictureBox1);
             this.btnHead.Controls.Add(this.guna2Button1);
             this.btnHead.Controls.Add(this.panelHasil);
             this.btnHead.Location = new System.Drawing.Point(330, 5);
@@ -148,14 +142,12 @@
             // panel3
             // 
             this.panel3.Controls.Add(this.button4);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.lblUser);
-            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Controls.Add(this.btnLogin);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(4, 5);
             this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(285, 129);
+            this.panel3.Size = new System.Drawing.Size(285, 66);
             this.panel3.TabIndex = 2;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
@@ -170,31 +162,17 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // lblUser
-            // 
-            this.lblUser.AutoSize = true;
-            this.lblUser.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUser.ForeColor = System.Drawing.Color.White;
-            this.lblUser.Location = new System.Drawing.Point(86, 38);
-            this.lblUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(99, 28);
-            this.lblUser.TabIndex = 1;
-            this.lblUser.Text = "Username";
-            this.lblUser.Click += new System.EventHandler(this.lblUser_Click);
-            // 
             // panelSidebar
             // 
             this.panelSidebar.AutoScroll = true;
             this.panelSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(146)))), ((int)(((byte)(119)))));
             this.panelSidebar.Controls.Add(this.panel3);
-            this.panelSidebar.Controls.Add(this.btnLogin);
             this.panelSidebar.Controls.Add(this.btnMaster);
             this.panelSidebar.Controls.Add(this.panelMaster);
             this.panelSidebar.Location = new System.Drawing.Point(8, 77);
             this.panelSidebar.Margin = new System.Windows.Forms.Padding(0, 0, 15, 0);
             this.panelSidebar.Name = "panelSidebar";
-            this.panelSidebar.Size = new System.Drawing.Size(326, 632);
+            this.panelSidebar.Size = new System.Drawing.Size(326, 681);
             this.panelSidebar.TabIndex = 6;
             this.panelSidebar.Paint += new System.Windows.Forms.PaintEventHandler(this.panelSidebar_Paint);
             // 
@@ -207,7 +185,7 @@
             this.btnLogin.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(146)))), ((int)(((byte)(119)))));
             this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(3, 142);
+            this.btnLogin.Location = new System.Drawing.Point(-3, 5);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(285, 49);
             this.btnLogin.TabIndex = 53;
@@ -223,7 +201,7 @@
             this.btnMaster.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(146)))), ((int)(((byte)(119)))));
             this.btnMaster.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnMaster.ForeColor = System.Drawing.Color.White;
-            this.btnMaster.Location = new System.Drawing.Point(3, 197);
+            this.btnMaster.Location = new System.Drawing.Point(3, 79);
             this.btnMaster.Name = "btnMaster";
             this.btnMaster.Size = new System.Drawing.Size(285, 49);
             this.btnMaster.TabIndex = 53;
@@ -233,12 +211,12 @@
             // panelMaster
             // 
             this.panelMaster.Controls.Add(this.guna2Button3);
+            this.panelMaster.Controls.Add(this.btnBarang);
             this.panelMaster.Controls.Add(this.btnPesanan);
             this.panelMaster.Controls.Add(this.guna2Button2);
-            this.panelMaster.Controls.Add(this.btnBarang);
             this.panelMaster.Controls.Add(this.panel4);
             this.panelMaster.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelMaster.Location = new System.Drawing.Point(4, 254);
+            this.panelMaster.Location = new System.Drawing.Point(4, 136);
             this.panelMaster.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelMaster.Name = "panelMaster";
             this.panelMaster.Size = new System.Drawing.Size(285, 291);
@@ -254,7 +232,7 @@
             this.btnPesanan.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(146)))), ((int)(((byte)(119)))));
             this.btnPesanan.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnPesanan.ForeColor = System.Drawing.Color.White;
-            this.btnPesanan.Location = new System.Drawing.Point(45, 228);
+            this.btnPesanan.Location = new System.Drawing.Point(53, 229);
             this.btnPesanan.Name = "btnPesanan";
             this.btnPesanan.Size = new System.Drawing.Size(237, 49);
             this.btnPesanan.TabIndex = 53;
@@ -270,7 +248,7 @@
             this.btnBarang.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(146)))), ((int)(((byte)(119)))));
             this.btnBarang.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnBarang.ForeColor = System.Drawing.Color.White;
-            this.btnBarang.Location = new System.Drawing.Point(53, 94);
+            this.btnBarang.Location = new System.Drawing.Point(53, 96);
             this.btnBarang.Name = "btnBarang";
             this.btnBarang.Size = new System.Drawing.Size(237, 49);
             this.btnBarang.TabIndex = 53;
@@ -318,7 +296,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(8, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(4, 5);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(309, 65);
@@ -326,35 +304,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Sewa_Alat_Camping_C15.Properties.Resources.profile_user__1_;
-            this.pictureBox2.Location = new System.Drawing.Point(4, 22);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(78, 85);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 0;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // guna2Button5
-            // 
-            this.guna2Button5.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button5.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button5.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(64)))), ((int)(((byte)(40)))));
-            this.guna2Button5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2Button5.ForeColor = System.Drawing.Color.White;
-            this.guna2Button5.Image = global::Sewa_Alat_Camping_C15.Properties.Resources.logout;
-            this.guna2Button5.Location = new System.Drawing.Point(8, 704);
-            this.guna2Button5.Name = "guna2Button5";
-            this.guna2Button5.Size = new System.Drawing.Size(327, 69);
-            this.guna2Button5.TabIndex = 53;
-            this.guna2Button5.Text = "Logout";
-            this.guna2Button5.Click += new System.EventHandler(this.guna2Button5_Click);
             // 
             // guna2Button2
             // 
@@ -386,19 +335,6 @@
             this.guna2Button3.TabIndex = 54;
             this.guna2Button3.Text = "Kurir";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(86, 65);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 28);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Administrator";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -409,23 +345,19 @@
             this.Controls.Add(this.btnHead);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelSidebar);
-            this.Controls.Add(this.guna2Button5);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
-            this.panel1.ResumeLayout(false);
             this.btnHead.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panelHsl.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.panelSidebar.ResumeLayout(false);
             this.panelMaster.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -442,8 +374,6 @@
         private System.Windows.Forms.Panel panelHsl;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label lblUser;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.FlowLayoutPanel panelSidebar;
         private System.Windows.Forms.Button button2;
         private Guna.UI2.WinForms.Guna2Button btnLogin;
@@ -452,10 +382,8 @@
         private Guna.UI2.WinForms.Guna2Button btnPesanan;
         private Guna.UI2.WinForms.Guna2Button btnBarang;
         private System.Windows.Forms.Panel panel4;
-        private Guna.UI2.WinForms.Guna2Button guna2Button5;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private Guna.UI2.WinForms.Guna2Button guna2Button3;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
-        private System.Windows.Forms.Label label2;
     }
 }
